945a01966c953230e75302b9ba09ff08c6f558cb
Sambolec <119959747+Sambolec@users.noreply.github.com>
2025-06-17 16:33:28 +0200

    Beta prez

--- Assets/ulazak_u_toranj.cs
+++ Assets/ulazak_u_toranj.cs
@@ -3,11 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class ulazak_u_toranj : MonoBehaviour
 {
-    public GameObject player; // Povuci Player objekt u Inspectoru
+    public GameObject player1; // Drag the first player object here in the Inspector
+    public GameObject player2; // Drag the second player object here in the Inspector
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == player)
+        if (other.gameObject == player1 || other.gameObject == player2)
         {
             SceneManager.LoadScene("drugi_level");
         }
